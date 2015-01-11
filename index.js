b69b7a97197417961f2fcc466ef88f8e80bea110
Greg Levine <glevine@sugarcrm.com>
2015-01-11 00:06:12 -0500

    Running `sugarcli test` creates Docker container with the host mounted to share a volume. As a proof of concept, the contents of that directory are printed and captured in node. Once the stream is redirected to stdout, the container is killed and removed. Nothing useful is done, but this is the framework for using Docker containers to execute processes.

--- index.js
+++ index.js
@@ -1,4 +1,58 @@
 #!/usr/bin/env node
 var app = require('commander');
 
-app.version('0.0.1').parse(process.argv);
+function handleError(action, err) {
+    if (err) {
+        console.error('error while ' + action + '...');
+        console.error(err);
+    }
+}
+
+app.version('0.1.0');
+
+app.command('test').action(function() {
+    var Docker = require('dockerode'),
+        docker = new Docker();
+
+    docker.createContainer({
+        Image: 'ubuntu',
+        Cmd: ['/bin/ls', '/tmp/app'],
+        Volumes: {
+            '/tmp/app': {}
+        }
+    }, function(err, container) {
+        console.log('attaching to... ' + container.id);
+
+        container.attach({stream: true, stdout: true, stderr: true, tty: true}, function(err, stream) {
+            handleError('attaching', err);
+
+            stream.pipe(process.stdout);
+
+            console.log('starting... ' + container.id);
+
+            container.start({
+                Binds: ['/Users/glevine/www:/tmp/app']
+            }, function(err, data) {
+                handleError('starting', err);
+            });
+
+            container.wait(function(err, data) {
+                handleError('waiting', err);
+
+                console.log('killing... ' + container.id);
+
+                container.kill(function(err, data) {
+                    handleError('killing', err);
+
+                    console.log('removing... ' + container.id);
+
+                    container.remove(function(err, data) {
+                        handleError('removing', err);
+                    });
+                });
+            });
+        });
+    });
+});
+
+app.parse(process.argv);
